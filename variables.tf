d97727239c8a870aa8894b14c93c150059346125
jansdhillon <jan.dhillon@canonical.com>
2025-09-18 23:34:40 -0600

    refactor/feat: improve alias/fingerprint handling, make instance type and remote configurable on a per-instance basis

--- variables.tf
+++ variables.tf
@@ -1,7 +1,24 @@
+variable "account_name" {
+  type        = string
+  description = "Landscape Server account name"
+}
+
 variable "architecture" {
   type        = string
   default     = "x86_64"
   description = "CPU architecture"
+}
+
+variable "fingerprint" {
+  type        = string
+  default     = null
+  description = "Default fingerprint of image. Can be overridden per instance."
+}
+
+variable "image_alias" {
+  type        = string
+  default     = null
+  description = "Default alias of image. Can be overridden per instance. Takes precedence over fingerprint if both are specified."
 }
 
 variable "instance_landscape_config_path" {
@@ -15,22 +32,15 @@ variable "instance_landscape_server_ssl_public_key_path" {
   default = "/etc/landscape/server.pem"
 }
 
-variable "registration_key" {
-  type     = string
-  nullable = true
-  default  = ""
-}
+variable "instance_type" {
+  type    = string
+  default = "container"
 
-variable "account_name" {
-  type        = string
-  description = "Landscape Server account name"
+  validation {
+    condition     = contains(["virtual-machine", "container"], var.instance_type)
+    error_message = "valid values are: virtual-machine, container"
+  }
 }
-
-variable "landscape_root_url" {
-  type        = string
-  description = "Landscape Server root URL"
-}
-
 
 variable "instances" {
   type = set(object({
@@ -39,33 +49,25 @@ variable "instances" {
       access_group             = optional(string)
       bus                      = optional(string)
       computer_title           = string
-      registration_key         = optional(string)
       data_path                = optional(string)
-      log_dir                  = optional(string)
-      log_level                = optional(string)
-      pid_file                 = optional(string)
-      ping_url                 = optional(string)
+      exchange_interval        = optional(number)
       include_manager_plugins  = optional(string)
       include_monitor_plugins  = optional(string)
+      log_dir                  = optional(string)
+      log_level                = optional(string)
+      package_hash_id_url      = optional(string)
+      pid_file                 = optional(string)
+      ping_interval            = optional(number)
+      ping_url                 = optional(string)
+      registration_key         = optional(string)
       script_users             = optional(string)
       ssl_public_key           = optional(string)
       tags                     = optional(string)
       url                      = optional(string)
-      package_hash_id_url      = optional(string)
-      exchange_interval        = optional(number)
       urgent_exchange_interval = optional(number)
-      ping_interval            = optional(number)
-
     })
-
-    fingerprint              = optional(string)
-    image_alias              = optional(string)
-    landscape_root_url       = optional(string)
-    landscape_client_package = optional(string)
-    http_proxy               = optional(string)
-    https_proxy              = optional(string)
-    additional_lxd_config    = optional(map(string))
-    additional_cloud_init    = optional(string)
+    additional_cloud_init = optional(string)
+    additional_lxd_config = optional(map(string))
     devices = optional(list(object({
       name       = string
       type       = string
@@ -92,6 +94,15 @@ variable "instances" {
       mode               = optional(string, "0755")
       create_directories = optional(bool, false)
     })), [])
+    fingerprint              = optional(string)
+    http_proxy               = optional(string)
+    https_proxy              = optional(string)
+    image_alias              = optional(string)
+    instance_type            = optional(string)
+    landscape_client_package = optional(string)
+    landscape_root_url       = optional(string)
+    profiles                 = optional(list(string))
+    remote                   = optional(string)
   }))
 
   validation {
@@ -102,14 +113,20 @@ variable "instances" {
   }
 }
 
-variable "instance_type" {
-  type    = string
-  default = "container"
+variable "landscape_client_package" {
+  type        = string
+  description = "Landscape Client debian package to install (ex. `landscape-client` or `landscape-client=23.02-0ubuntu1~22.04.5`)"
+  default     = "landscape-client"
+}
 
-  validation {
-    condition     = contains(["virtual-machine", "container"], var.instance_type)
-    error_message = "valid values are: virtual-machine, container"
-  }
+variable "landscape_root_url" {
+  type        = string
+  description = "Landscape Server root URL"
+}
+
+variable "ppa" {
+  type    = string
+  default = null
 }
 
 variable "pro_token" {
@@ -118,41 +135,31 @@ variable "pro_token" {
   sensitive   = true
 }
 
-variable "ppa" {
-  type    = string
-  default = null
+variable "profiles" {
+  type        = list(string)
+  description = "Profiles to associate with all instances."
+  default     = ["default"]
 }
 
-variable "landscape_client_package" {
-  type        = string
-  description = "Landscape Client debian package to install (ex. `landscape-client` or `landscape-client=23.02-0ubuntu1~22.04.5`)"
-  default     = "landscape-client"
-}
-
-variable "fingerprint" {
-  type        = string
-  default     = null
-  description = "Default fingerprint of image. Can be overridden per instance."
-}
-
-variable "image_alias" {
-  type        = string
-  default     = null
-  description = "Default alias of image. Can be overridden per instance. Takes precedence over fingerprint if both are specified."
+variable "registration_key" {
+  type     = string
+  nullable = true
+  default  = ""
 }
 
 variable "remote" {
-  type    = string
-  default = "ubuntu"
-}
-
-variable "wait_for_cloud_init" {
-  type    = bool
-  default = true
+  type        = string
+  default     = "ubuntu"
+  description = "global image remote"
 }
 
 variable "timeout" {
   type        = string
   description = "duration string to wait for instances to deploy"
   default     = "10m"
+}
+
+variable "wait_for_cloud_init" {
+  type    = bool
+  default = true
 }
