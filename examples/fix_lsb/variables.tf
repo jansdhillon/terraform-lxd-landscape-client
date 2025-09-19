3d2e658def99c2359622d9d3aa36bb16c6f04271
jansdhillon <jan.dhillon@canonical.com>
2025-09-19 08:05:42 -0600

    refactor: only setup with lxd exec (no cloud init), rm yaml merge

--- examples/fix_lsb/variables.tf
+++ examples/fix_lsb/variables.tf
@@ -17,7 +17,8 @@ variable "landscape_root_url" {
 variable "registration_key" {
   description = "Landscape Server registration key"
   type        = string
-  sensitive   = true
+  nullable    = true
+  default     = ""
 }
 
 variable "landscape_client_package" {
@@ -28,14 +29,46 @@ variable "landscape_client_package" {
 
 variable "instances" {
   type = set(object({
-    image_alias              = optional(string)
-    landscape_client_package = optional(string)
     client_config = object({
+      account_name             = optional(string)
+      access_group             = optional(string)
+      bus                      = optional(string)
       computer_title           = string
-      ping_interval            = optional(number)
+      data_path                = optional(string)
       exchange_interval        = optional(number)
+      include_manager_plugins  = optional(string)
+      include_monitor_plugins  = optional(string)
+      log_dir                  = optional(string)
+      log_level                = optional(string)
+      package_hash_id_url      = optional(string)
+      pid_file                 = optional(string)
+      ping_interval            = optional(number)
+      ping_url                 = optional(string)
+      registration_key         = optional(string)
+      script_users             = optional(string)
+      ssl_public_key           = optional(string)
+      tags                     = optional(string)
+      url                      = optional(string)
       urgent_exchange_interval = optional(number)
     })
+    lxd_config = optional(map(string))
+    devices = optional(list(object({
+      name       = string
+      type       = string
+      properties = map(string)
+    })), [])
+    execs = optional(list(object({
+      name          = string
+      command       = list(string)
+      enabled       = optional(bool, true)
+      trigger       = optional(string, "on_change")
+      environment   = optional(map(string))
+      working_dir   = optional(string)
+      record_output = optional(bool, false)
+      fail_on_error = optional(bool, false)
+      uid           = optional(number, 0)
+      gid           = optional(number, 0)
+    })), [])
     files = optional(list(object({
       content            = optional(string)
       source_path        = optional(string)
@@ -45,7 +78,18 @@ variable "instances" {
       mode               = optional(string, "0755")
       create_directories = optional(bool, false)
     })), [])
-    additional_cloud_init = optional(string)
+    fingerprint                = optional(string)
+    http_proxy                 = optional(string)
+    https_proxy                = optional(string)
+    image_alias                = optional(string)
+    instance_type              = optional(string)
+    server_ssl_public_key_path = optional(string, "/etc/landscape/server.pem")
+    landscape_client_package   = optional(string)
+    landscape_root_url         = optional(string)
+    ppa                        = optional(string)
+    profiles                   = optional(list(string))
+    pro_token                  = optional(string)
+    remote                     = optional(string)
   }))
 }
 
--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,13 @@ output "status" {
     for idx, instance in lxd_instance.instance : idx => instance.status
   }
 }
+
+output "setup_output" {
+  value = {
+    for idx, instance in lxd_instance.instance : idx => {
+      code = instance.execs["001-setup"].exit_code
+      out  = instance.execs["001-setup"].stdout
+      err  = instance.execs["001-setup"].stderr
+    }
+  }
+}
